a405388b75dc3de395c182b075e38a7c3c399876
Chris Seifried <seifried.chris@gmail.com>
2017-07-20 13:35:20 -0400

    Fix string matching, output, cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -49,65 +49,59 @@ fn main() {
     let mut user = build_player();
     let mut dealer = build_player();
 
-    let mut is_running = true;
     let mut user_turn = true;
-   // let mut input = String::new();
 
     display_hands(&user, &dealer);
     let mut input = get_move();
 
-    if input == "stand".to_string() { user_turn = false; }
+    if input == "stand" { user_turn = false; }
 
-    while is_running {
+    loop {
         while user_turn {
             user.draw_card();
+            display_hands(&user, &dealer);
 
             if user.total > 21 {
-                display_hands(&user, &dealer);
-                println!(" BUSTED!\nYou busted. Dealer wins.");
-                is_running = false;
-                break;
+                println!("BUSTED!\nYou busted. Dealer wins.");
+                return;
             }
 
-            // Get input
-            display_hands(&user, &dealer);
+            // Get decision from user
             input = get_move();
-
-            if input == "stand".to_string() { user_turn = false; }
+            if input == "stand" { user_turn = false; }
         }
 
-        if dealer.total >= 17 && is_running {
+        if dealer.total >= 17 {
             // Determing who wins if no one busts
-            println!("\n\nDealer stands.\n\n");
-            println!("The dealer:\n{} + {} = {}\n\n", dealer.hand, dealer.new_card, dealer.total);
-            println!("You:\n{} + {} = {}", user.hand, user.new_card, user.total);
+            println!("Dealer stands.\n");
+            println!("The dealer:\n{} + {} = {}\n", dealer.hand, dealer.new_card, dealer.total);
+            println!("You:\n{} + {} = {}\n", user.hand, user.new_card, user.total);
 
             if dealer.total > user.total {
-                println!("\n\nDealer wins.\n");
+                println!("Dealer wins.\n");
             } else if dealer.total < user.total {
-                println!("\n\nYou win!\n");
+                println!("You win!\n");
             } else {
-                println!("\n\nTie! (Push)\n");
+                println!("Tie! (Push)\n");
             }
 
-            is_running = false;
-            break;
+            return;
         }
 
         else {
             // Dealer's turn - loop
-            while dealer.total < 17 && is_running {
-                println!("\n\nDealer hits.\n\n");
+            while dealer.total < 17 {
+                println!("\nDealer hits.\n");
                 dealer.draw_card();
 
                 // The dealer busts
                 if dealer.total > 21 {
                     println!("The dealer:\n{} + {} = {}", dealer.hand, dealer.new_card, dealer.total);
-                    println!(" BUSTED!\n\n");
+                    println!("BUSTED!\n\n");
                     println!("You:\n{} + {} = {}\n\n", user.hand, user.new_card, user.total);
                     println!("The dealer busted. You win!\n");
-                    is_running = false;
-                    break;
+
+                    return;
                 }
 
                 display_hands(&user, &dealer);
@@ -118,7 +112,7 @@ fn main() {
 
 fn display_hands(usr: &Player, dlr: &Player) {
     println!("The dealer:\n? + {}\n", dlr.new_card);
-    println!("You:\n{} + {} = {}", usr.hand, usr.new_card, usr.total);
+    println!("You:\n{} + {} = {}\n", usr.hand, usr.new_card, usr.total);
 }
 
 // Used to return a new default instance of a player
@@ -137,18 +131,14 @@ fn build_player() -> Player {
 }
 
 fn get_move() -> String {
-    println!("\n\n");
     let mut input = String::new();
     loop {
-        println!("would you like to \"hit\" or \"stand\"?");
-        io::stdin().read_line(&mut input);
+        println!("Would you like to \"hit\" or \"stand\"?");
+        io::stdin().read_line(&mut input).ok();
         input = String::from(input.trim());
         match input.as_ref() {
-            "hit"   => break,
-            "stand" => break,
-            _       => input = String::new(),
+            "hit" | "stand" => return input,
+                           _ => input = String::new(),
         }
     }
-
-    return input;
 }
